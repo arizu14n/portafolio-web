6b55d04f5e905f6f0800c2597e5eb2373b0c9c9b
arizu14n <arizu14n@gmail.com>
2026-01-11 16:45:33 -0300

    cambio frase principal

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = ({ isHomeContentFlyingAway }) => {
   const [typedCode, setTypedCode] = useState('');
   const [currentPhraseStyle, setCurrentPhraseStyle] = useState({});
 
-  const leadPhrase = "Apasionado por el aprendizaje continuo y la creación de aplicaciones funcionales.";
+  const leadPhrase = "Integro conocimiento contable y tecnológico para desarrollar software que optimiza procesos y genera valor.";
   const leadWords = leadPhrase.split(' '); // Split into words
 
   const [currentLeadWordIndex, setCurrentLeadWordIndex] = useState(0);
