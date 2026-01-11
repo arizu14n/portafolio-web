95343f79649209841acf82c3a9e832e2b277eb74
arizu14n <arizu14n@gmail.com>
2026-01-11 19:17:02 -0300

    agrego un link de una Demo

--- src/components/Demos.js
+++ src/components/Demos.js
@@ -18,8 +18,8 @@ const Demos = () => {
                 'Control de caja y tablero de control'
             ],
             technologies: ['React', 'FastAPI', 'PostgreSQL', 'Docker'],
-            youtubeId: '', // Placeholder - agregar ID del video de YouTube
-            thumbnail: 'https://img.youtube.com/vi/YOUTUBE_ID/maxresdefault.jpg'
+            youtubeId: 'JhexoqrJytk',
+            thumbnail: 'https://img.youtube.com/vi/JhexoqrJytk/maxresdefault.jpg'
         },
         {
             id: 2,
